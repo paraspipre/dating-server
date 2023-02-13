4f6c792987d1b5fc85e6e32106bcc6b1ded8d7db
paras11917 <70134657+paras11917@users.noreply.github.com>
2023-02-13 16:04:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ const server = app.listen(process.env.PORT, () =>
 
 const io = socket(server, {
    cors: {
-      origin: "http://localhost:3000"
+      origin: "https://dating-frontend-pi.vercel.app/"
    },
 });
 
